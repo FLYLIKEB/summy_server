f4b6b5195f8ec967549fb648d815498574eb486f
FLYLIKEB <rerub0831@gmail.com>
2025-04-08 22:01:01 +0900

    fix: 통합 테스트용 TestApplication 클래스 이동

---
+++ core/src/integrationTest/java/com/jwp/core/TestApplication.java
@@ -0,0 +1,14 @@
+package com.jwp.core;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * 코어 모듈 테스트를 위한 스프링 부트 설정 클래스
+ */
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.jwp.core.repository")
+@EntityScan(basePackages = "com.jwp.core.domain")
+public class TestApplication {
+} 
