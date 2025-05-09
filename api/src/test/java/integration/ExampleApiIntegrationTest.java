6cd3bb6a2f1e385dc8889c733873b0a20c107cc0
FLYLIKEB <rerub0831@gmail.com>
2025-05-09 14:08:46 +0900

    refactor: @MockBean을 @Mock으로 교체

--- api/src/test/java/integration/ExampleApiIntegrationTest.java
+++ api/src/test/java/integration/ExampleApiIntegrationTest.java
@@ -2,8 +2,10 @@ package integration;
 
 import com.jwp.api.ApiApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -16,6 +18,7 @@ import org.springframework.test.context.ActiveProfiles;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(classes = ApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(MockitoExtension.class)
 @ActiveProfiles("test")
 public class ExampleApiIntegrationTest {
     
@@ -24,16 +27,16 @@ public class ExampleApiIntegrationTest {
     
     private TestRestTemplate restTemplate = new TestRestTemplate();
     
-    @MockBean
+    @Mock
     private UserRepository userRepository;
     
-    @MockBean
+    @Mock
     private PasswordEncoder passwordEncoder;
     
-    @MockBean
+    @Mock
     private UserCommandService userCommandService;
     
-    @MockBean
+    @Mock
     private UserQueryService userQueryService;
     
     @Test
